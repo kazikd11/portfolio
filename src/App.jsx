adf6893a9649a32fcdda2dba965224708ae1fb06
Michał Kaźmierczak <kmiki246@gmail.com>
2025-02-13 21:34:24 +0100

    temp background (tbd api images)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import React, { useState } from 'react'
-
-import ProjectSection from './components/ProjectSection'
-import ProjectWindow from './components/ProjectWindow'
+import React, { useState } from 'react';
+import ProjectSection from './components/ProjectSection';
+import ProjectWindow from './components/ProjectWindow';
 import Navbar from './components/Navbar';
-
-import sections from './data/projects.json'
-
+import sections from './data/projects.json';
 
 const App = () => {
-	const [openedProject, setOpenedProject] = useState(null)
-	const [windowPosition, setWindowPostion] = useState(null)
+	const [openedProject, setOpenedProject] = useState(null);
+	const [windowPosition, setWindowPostion] = useState(null);
 
 	const handleCardDoubleClick = (proj, pos) => {
-		setOpenedProject(proj)
-		setWindowPostion(pos)
+		setOpenedProject(proj);
+		setWindowPostion(pos);
 	};
 
 	const handleCloseProject = () => {
-		setOpenedProject(null)
+		setOpenedProject(null);
 	};
 
 	return (
-		<div className="min-h-screen bg-secondary">
+		<div className="min-h-screen bg-primary relative">
+			<div className="absolute inset-0 bg-custom-image opacity-20 z-0"></div>
 
 			<Navbar />
 
-			<div className="custom-scroll overflow-auto h-[100vh] pt-8 pb-20">
-			{sections.map((section, i) => (
-				<ProjectSection
-					key={i}
-					title={section.title}
-					projects={section.projects}
-					handleCardDoubleClick={handleCardDoubleClick}
-				/>
-			))}
+			<div className="custom-scroll overflow-auto h-[100vh] pt-8 pb-20 relative z-10">
+				{sections.map((section, i) => (
+					<ProjectSection
+						key={i}
+						title={section.title}
+						projects={section.projects}
+						handleCardDoubleClick={handleCardDoubleClick}
+					/>
+				))}
 			</div>
 
 			{openedProject && (
