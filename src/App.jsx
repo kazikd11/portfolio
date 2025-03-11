5eb3b3e827751e45d0269c1fe3bbc6b5a3a79d47
Michał Kaźmierczak <kmiki246@gmail.com>
2025-03-11 23:14:08 +0100

    scroll lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import ProjectSection from './components/ProjectSection';
 import ProjectWindow from './components/ProjectWindow';
 import Navbar from './components/Navbar';
@@ -10,6 +10,15 @@ const App = () => {
 
 	const [openedProject, setOpenedProject] = useState(null);
 	const [windowPosition, setWindowPostion] = useState(null);
+	const [isScrollDisabled, setIsScrollDisabled] = useState(true);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setIsScrollDisabled(false);
+		}, 7000);
+
+		return () => clearTimeout(timeout);
+	}, []);
 
 	const handleCardDoubleClick = (proj, pos) => {
 		setOpenedProject(proj);
@@ -23,12 +32,12 @@ const App = () => {
 	return (
 		<div className="min-h-screen bg-primary relative">
 			<div className="absolute inset-0 bg-custom-image opacity-20 z-0"></div>
-			
-			{(window.self === window.top) && <Cursor buttonRef={buttonRef}/>}
-			
+
+			{window.self === window.top && <Cursor buttonRef={buttonRef} />}
+
 			<Navbar />
 
-			<div className="custom-scroll overflow-auto h-[100vh] pt-8 pb-20 relative z-10">
+			<div className={`custom-scroll h-[100vh] pt-8 pb-20 relative z-10 ${isScrollDisabled && window.self === window.top ? 'overflow-hidden' : 'overflow-auto'}`}>
 				{sections.map((section, i) => (
 					<ProjectSection
 						key={i}
