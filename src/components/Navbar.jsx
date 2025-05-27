4ad9d0e7882458d84364565fb223efe3bc272e70
Michał Kaźmierczak <kmiki246@gmail.com>
2025-05-27 16:49:13 +0200

    center icons in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,38 @@ import Icons from "./Icons";
 
 const Navbar = () => {
     return (
-        <nav className="bg-primary flex items-center justify-between text-3xl font-bold p-4 sticky w-full z-10 h-24">
-            <motion.p whileHover={{ scale: 1.1 }} className="ml-4">
+        <nav className="bg-primary flex items-center justify-between text-3xl font-bold p-4 sticky w-full z-10 h-24 relative">
+            <motion.p whileHover={{ scale: 1.1 }} className="ml-4 z-20">
                 Michał Kaźmierczak
             </motion.p>
-            <Icons className=""/>
-            <div className="flex space-x-4 mr-4">
-                <a
+            <div className="absolute left-0 right-0 flex justify-center z-10">
+                <Icons />
+            </div>
+            <div className="flex space-x-4 mr-4 z-20">
+                <motion.a
+                    whileHover={{ scale: 1.1 }}
                     href="https://github.com/kazikd11"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <FaGithub className="icon hover:text-gray-500" />
-                </a>
-                <a
+                </motion.a>
+                <motion.a
+                    whileHover={{ scale: 1.1 }}
                     href="https://www.linkedin.com/in/kazikd11"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <FaLinkedin className="icon hover:text-blue-400" />
-                </a>
-                <a
+                </motion.a>
+                <motion.a
+                    whileHover={{ scale: 1.1 }}
                     href="/CV_Michal_Kazmierczak.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <FaFilePdf className="icon hover:text-red-500" />
-                </a>
+                </motion.a>
             </div>
         </nav>
     );
